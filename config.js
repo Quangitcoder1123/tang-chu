48a0cfe44776e43c4691aa0147c7bfe8ef23c47a
Quangitcoder1123 <94438536+Quangitcoder1123@users.noreply.github.com>
2021-11-16 21:02:07 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐 Hello TRINH XAM ! 🖐',
+    introDesc: `TUI có điều này muốn hỏi CHÚ, CHÚ nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'TUI biết CHÚ có người yêu rồi phải không phải không ? 😙',
+    desc: 'CHÚ không trả lời hoặc thoát ra, tức là CHÚ thích AI đó nha :> ',
+    btnYes: 'Có, CHÚ thật sự có người yêu 💖💖💖',
+    btnNo: '💔 Có cái nịt nè ! 💔 ',
+    question:'Nói cho TUI biết đi vì sao CHÚ có người yêu rồi đi 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì tao thích gái đó Ahiiiii 😊😊',
+    mess: 'TUI biết mà, CHÚ CÓ NGƯỜI YÊU RỒI. HUHU CHÚC CHÚ HẠNH PHÚC  300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho TUI nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.facebook.com/profile.php?id=100022481742599'
+}
